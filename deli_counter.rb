1e23075e95121eb81f302e524386d4feaa6c241c
Julie Meng <julieannmeng@gmail.com>
2021-09-26 19:23:45 -0400

    commit tree b3cd75ee9d1dd321b6fb126fbb151e11862f3deb

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+require 'pry'
+deli_line = [];
+
+def line
+    if (deli_line[0] == "") {
+        puts "The line is currently empty.";
+    } else {
+        deli_line.filter {|person| puts person.index + 1};
+    }
+end
+end
+binding.pry
